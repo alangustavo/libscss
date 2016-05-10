f6415ce2d09ac874e9d15c70596977fdc7f88192
alan <alan@alan-Lenovo-B570>
2016-05-10 10:57:15 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Testar a sequencia de carga dos arquivos do Bootstrap, Angular, JQuery e FontAwesome
+ */
+?><html lang="pt">
+    <head>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" >
+        <!-- Tema Opcional -->
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css" >
+        <!-- JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        
+        <!-- FontAwesome -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        
+        <!-- JQuery -->
+        <script src="js/jquery-2.2.3.min.js"></script>
+        
+        <!-- AngularJS -->
+        <script src="js/angular.min.js"></script>
+        
+
+    </head>
+    <body>
+        <h1>A inclusão de todas as Bibliotecas foi testada</h1>
+        
+    </body>
+    
+</html>
